4f4491db240cddb25446c28e32f495ec0162e5e4
Chen Liang <chen.liang.mail@protonmail.com>
2022-02-07 19:57:33 +0000

    Initial commit

---
+++ .emacs.d/elpa/apheleia-20220114.2329/apheleia-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from apheleia.el  -*- no-byte-compile: t -*-
+(define-package "apheleia" "20220114.2329" "Reformat buffer stably" '((emacs "25.2")) :commit "75074a2f11e29aeca376b3004270a93ce44e59de" :authors '(("Radon Rosborough" . "radon.neon@gmail.com")) :maintainer '("Radon Rosborough" . "radon.neon@gmail.com") :keywords '("tools") :url "https://github.com/raxod502/apheleia")
---
+++ .emacs.d/transient/history.el
@@ -0,0 +1,7 @@
+((magit-commit nil)
+ (magit-diff
+  (("--" "minigrep_trait/src/main.rs"))
+  (("--" "minigrep_trait/src/lib.rs")))
+ (magit-dispatch nil)
+ (magit-push nil)
+ (magit-status-jump nil))
