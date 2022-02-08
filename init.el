07416fefa8cdb8c6cc62b26c64bcee1ee83aed28
Chen Liang <chen.liang.mail@protonmail.com>
2022-02-08 11:10:06 +0000

    Ensure package are installed. Add ignore file

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; package --- Summary
 ;;; Commentary:
-
+;; Emacs configuration by Chen Liang
 
 ;;; Code:
 ;; Set up package.el to work with MELPA
@@ -15,6 +15,7 @@
   (package-install 'use-package))
 
 (use-package evil :ensure)
+(use-package magit :ensure)
 ;; (require 'evil)
 ;; (evil-mode 0)
 
@@ -26,7 +27,7 @@
  '(custom-safe-themes
    '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "9b4ae6aa7581d529e20e5e503208316c5ef4c7005be49fdb06e5d07160b67adc" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" default))
  '(package-selected-packages
-   '(js2-refactor xref-js2 js2-mode company flycheck lsp-ui apheleia lsp-mode flymake-aspell magit web-mode rust-mode one-themes)))
+   '(one-theme js2-refactor xref-js2 js2-mode company flycheck lsp-ui apheleia lsp-mode flymake-aspell magit web-mode rust-mode one-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -34,13 +35,13 @@
  ;; If there is more than one, they won't work right.
  )
 
-(use-package atom-one-dark-theme
+(use-package one-themes
   :ensure t
   :load-path "themes"
   :init
-  (setq molokai-theme-kit t)
+  ;; (setq one-dark-theme-kit t)
   :config
-  (load-theme 'atom-one-dark t)
+  (load-theme 'one-dark t)
   )
 ;; (load-theme 'atom-one-dark t)
 
@@ -74,6 +75,7 @@
 
 ;; turn on semantic
 ;; (semantic-mode 1)
+(use-package auto-complete :ensure)
 
 (use-package lsp-mode
   :ensure
@@ -87,9 +89,12 @@
   :config
   (add-hook 'lsp-mode-hook 'lsp-ui-mode)
   (add-hook 'lsp-mode-hook 'flyspell-prog-mode)
+  (add-hook 'lsp-mode-hook 'auto-complete-mode)
   (add-hook 'js2-mode-hook 'lsp)
   (add-hook 'css-mode 'lsp)
-  (add-hook 'web-mode 'lsp))
+  (add-hook 'web-mode 'lsp)
+  (add-hook 'c-mode 'lsp)
+  (add-hook 'c++-mode 'lsp))
 
 (use-package js2-mode :ensure)
 
