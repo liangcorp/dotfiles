6dc9f197689e2c8a55babbdeb5964b7d55352178
Chen Liang <chen.liang@ie.ibm.com>
2023-02-01 02:18:51 +0000

    Convert vim script to lua

---
+++ nvim/init.lua
@@ -0,0 +1,69 @@
+vim.cmd.nocompatible = true
+vim.cmd.wildmenu = true
+vim.cmd.ruler = true
+vim.cmd([[
+    syntax on
+    filetype plugin indent on
+    set tabstop=4
+    set softtabstop=0
+    set shiftwidth=4
+    set expandtab
+    set path+=**
+    set mouse=a
+    set mousemodel=popup
+    set guioptions=egmrti
+    set gfn=Monospace\ 10
+    set autowrite
+    set encoding=UTF-8
+    set completeopt=longest
+    
+    colorscheme dracula
+
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    augroup end
+]])
+
+vim.opt.number = true
+
+-- Packer
+local use = require('packer').use
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' 
+end)
+
+require('plugins')
+
+-- Git signs
+require('gitsigns').setup()
+
+-- Treesitter
+require("nvim-tree").setup()
+require("tree")
+
+-- LSP Configurations
+require'lspconfig'.pyright.setup{}
+require("lspmain")
+require("rust")
+require("go")
+require("python")
+
+-- Telescope
+require("telescopeconfig")
+
+-- Prettier
+require("prettierconfig")
+
+-- Status line using lualine
+require('lualine').setup()
+
+-- coc.nvim
+require('cocconfig')
+
+-- Debugger vimspector
+vim.cmd([[
+let g:vimspector_enable_mappings = 'HUMAN'
+let g:vimspector_install_gadgets = [ 'debugpy', 'vscode-cpptools', 'CodeLLDB', 'delve' ]
+]])
+
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,32 @@
+return require('packer').startup(function(use)
+  use('wbthomason/packer.nvim')
+  use('sheerun/vim-polyglot')
+  use('rust-lang/rust.vim')
+  use('prabirshrestha/vim-lsp')
+  use('neoclide/coc.nvim')
+  use('nvim-lua/plenary.nvim')
+  use('nvim-telescope/telescope.nvim')
+  use('nvim-telescope/telescope-symbols.nvim')
+  use('nvim-treesitter/nvim-treesitter')
+  use('lewis6991/gitsigns.nvim')
+  use('kyazdani42/nvim-tree.lua')
+  use('tpope/vim-fugitive')
+
+  use('Mofiqul/dracula.nvim')
+
+-- lualine status line
+  use('kyazdani42/nvim-web-devicons') -- optional, for file icons
+  use('nvim-lualine/lualine.nvim')
+
+-- Track the engine.
+  use('SirVer/ultisnips')
+
+-- Snippets are separated from the engine. Add this if you want them:
+  use('neovim/nvim-lspconfig')
+  use('jose-elias-alvarez/null-ls.nvim')
+  use('MunifTanjim/prettier.nvim')
+
+-- Debugger
+  -- use('mfussenegger/nvim-dap')
+  use('puremourning/vimspector')
+end)
