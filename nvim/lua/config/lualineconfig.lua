30e01c94cc41de343ccc8eeb4fc93a9fb4030bc9
Chen Liang <chen.liang@ie.ibm.com>
2023-02-19 03:44:36 +0000

    Use default lualine setting

--- nvim/lua/config/lualineconfig.lua
+++ nvim/lua/config/lualineconfig.lua
@@ -1,11 +1,10 @@
-require('lualine').setup {
-    sections = {
-        lualine_b = {
-            {'branch'},
-            {'diff'},
-            {'diagnostics', sources={ 'coc' }}
-        }
-    }
-}
--- require('lualine').setup {}
-
+-- require('lualine').setup {
+--     sections = {
+--         lualine_b = {
+--             {'branch'},
+--             {'diff'},
+--             {'diagnostics', sources={ 'coc' }}
+--         }
+--     }
+-- }
+require('lualine').setup {}
