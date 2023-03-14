09fc277fde931b85fe096bdaddb2c29f9c638f4d
Chen Liang <chen.liang@ie.ibm.com>
2023-03-14 00:45:04 +0000

    Switch to lua function

--- nvim/lua/config/aerialline.lua
+++ nvim/lua/config/aerialline.lua
@@ -2,8 +2,11 @@ require('aerial').setup({
     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
     on_attach = function(bufnr)
         -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
-        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-        vim.keymap.set('n', '<leader>fa', ':Telescope aerial<CR>', { buffer = bufnr })
+        local aerial = require('aerial')
+        vim.keymap.set('n', '{', function() aerial.prev() end, { buffer = bufnr })
+        vim.keymap.set('n', '}', function() aerial.next() end, { buffer = bufnr })
+        vim.keymap.set('n', '<leader>fa', function() require('telescope').extensions.aerial.aerial() end,
+            { buffer = bufnr }
+        )
     end
 })
--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -24,6 +24,8 @@ vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "System Paste" })
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+local opts = { noremap = true, silent = true }
+
 -- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
@@ -35,85 +37,66 @@ vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current 
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
 
 -- Neotest
--- local neotest = require("neotest")
-vim.keymap.set("n", "<leader>tt", ":lua require('neotest').run.run()<CR>)",
-    { desc = 'Run Nearest [T]est' }
-)
-vim.keymap.set("n", "<leader>tf", ":lua require('neotest').run.run(vim.fn.expand(\"%\"))<CR>",
-    { desc = '[T]est Current File' }
-)
-vim.keymap.set("n", "<leader>ts", ":lua require('neotest').run.stop()<CR>",
-    { desc = '[S]top [T]est' }
-)
-vim.keymap.set("n", "<leader>ta", ":lua require('neotest').run.attach()<CR>",
-    { desc = '[T]est [A]ttrach' }
-)
-vim.keymap.set("n", "<leader>to", ":lua require('neotest').output.open({ enter = false })<CR>",
-    { desc = '[O]pen Output' }
-)
-vim.keymap.set("n", "<leader>tp", ":lua require('neotest').output_panel.open()<CR>",
-    { desc = 'Open [T]est [P]anel' }
-)
-vim.keymap.set("n", "<leader>tP", ":lua require('neotest').output_panel.close()<CR>",
-    { desc = 'Close [T]est [P]anel' }
-)
+local neotest = require("neotest")
+vim.keymap.set("n", "<leader>tt", function() neotest.run.run() end, { desc = 'Run Nearest [T]est' })
+vim.keymap.set("n", "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end, { desc = '[T]est Current File' })
+vim.keymap.set("n", "<leader>ts", function() neotest.run.stop() end, { desc = '[S]top [T]est' })
+vim.keymap.set("n", "<leader>ta", function() neotest.run.attach() end, { desc = '[T]est [A]ttrach' })
+vim.keymap.set("n", "<leader>to", function() neotest.output.open({ enter = false }) end, { desc = '[O]pen Output' })
+vim.keymap.set("n", "<leader>tp", function() neotest.output_panel.open() end, { desc = 'Open [T]est [P]anel' })
+vim.keymap.set("n", "<leader>tP", function() neotest.output_panel.close() end, { desc = 'Close [T]est [P]anel' })
 
 -- Trouble
-vim.keymap.set("n", "<leader>xx", ":TroubleToggle<CR>",
-    { desc = "Trouble Toggle" }
-)
-vim.keymap.set("n", "<leader>xw", ":TroubleToggle workspace_diagnostics<CR>",
-    { desc = "Trouble Toggle in Workspace" }
-)
-vim.keymap.set("n", "<leader>xd", ":TroubleToggle document_diagnostics<CR>",
-    { desc = "Trouble Toggle in Document" }
-)
-vim.keymap.set("n", "<leader>xl", ":TroubleToggle loclist<CR>",
-    { desc = "Trouble Toggle Loclist" }
-)
-vim.keymap.set("n", "<leader>xq", ":TroubleToggle quickfix<CR>",
-    { desc = "Trouble Toggle Quickfix" }
-)
-vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<CR>",
-    { desc = "Trouble Toggle LSP References" }
-)
+local trouble = require('trouble')
+vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end, { desc = "Trouble Toggle" })
+-- vim.keymap.set("n", "<leader>xx", ":TroubleToggle<CR>", { desc = "Trouble Toggle" })
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts)
+-- vim.keymap.set("n", "<leader>xw", function() trouble.toggle().workspace_diagnostics() end, { desc = "Trouble Toggle in Workspace" })
+vim.keymap.set("n", "<leader>xd", ":TroubleToggle document_diagnostics<CR>", { desc = "Trouble Toggle in Document" })
+-- vim.keymap.set("n", "<leader>xd", function() trouble.toggle().mode().document_diagnostics() end, { desc = "Trouble Toggle in Document" })
+vim.keymap.set("n", "<leader>xl", ":TroubleToggle loclist<CR>", { desc = "Trouble Toggle Loclist" })
+-- vim.keymap.set("n", "<leader>xl", function() trouble.toggle().loclist() end, { desc = "Trouble Toggle Loclist" })
+vim.keymap.set("n", "<leader>xq", ":TroubleToggle quickfix<CR>", { desc = "Trouble Toggle Quickfix" })
+-- vim.keymap.set("n", "<leader>xq", function() trouble.toggle().quickfix() end, { desc = "Trouble Toggle Quickfix" })
+vim.keymap.set("n", "gR", ":TroubleToggle lsp_references<CR>", { desc = "Trouble Toggle LSP References" })
+-- vim.keymap.set("n", "gR", function() trouble.toggle().lsp_references() end, { desc = "Trouble Toggle LSP References" })
 
 -- Arial related shortcuts
-vim.keymap.set('n', '<leader>ae', ':AerialToggle!right<CR>', { desc = '[A]erial Toggle' })
+local aerial = require('aerial')
+vim.keymap.set('n', '<leader>ae', function() aerial.toggle() end, { desc = '[A]erial Toggle' })
 
 -- Prettier
-vim.keymap.set('n', '<space>p', ':Prettier<CR>', { desc = '[P]rettier' })
+local prettier = require('prettier')
+vim.keymap.set('n', '<space>p', function() prettier.format() end, { desc = '[P]rettier' })
 
 -- NVIM Tree
-vim.keymap.set('n', "<leader>zz", ":NvimTreeToggle<CR>", { desc = "Nvim Tree" })
+local nvim_tree = require('nvim-tree')
+vim.keymap.set('n', "<leader>zz", function() nvim_tree.toggle() end, { desc = "Nvim Tree" })
 
 -- Debugger related shortcuts
-vim.keymap.set("n", "<F3>", ":lua require'dap'.close()<CR>", { desc = 'Close Debugger' })
-vim.keymap.set("n", "<F4>", ":lua require'dap'.restart()<CR>", { desc = 'Restart Debugger' })
+local dap = require('dap')
+vim.keymap.set("n", "<F3>", function() dap.close() end, { desc = 'Close Debugger' })
+vim.keymap.set("n", "<F4>", function() dap.restart() end, { desc = 'Restart Debugger' })
 
-vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>", { desc = 'Continue Debugger' })
-vim.keymap.set("n", "<F9>", ":lua require'dap'.toggle_breakpoint()<CR>", { desc = 'Set Breakpoint' })
-vim.keymap.set("n", "<leader><F9>", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+vim.keymap.set("n", "<F5>", function() dap.continue() end, { desc = 'Continue Debugger' })
+vim.keymap.set("n", "<F9>", function() dap.toggle_breakpoint() end, { desc = 'Set Breakpoint' })
+vim.keymap.set("n", "<leader><F9>", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end,
     { desc = 'Set Breakpoint with Condition' }
 )
-vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>", { desc = 'Step Over' })
-vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>", { desc = 'Step Into' })
-vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>", { desc = 'Step Out' })
-vim.keymap.set("n", "<F6>", ":lua require'dap'.repl.open()<CR>", { desc = 'Replay Open' })
-vim.keymap.set("n", "<leader>dc", ":lua require'dapui'.close()<CR>", { desc = 'Close Debugger UI' })
+vim.keymap.set("n", "<F10>", function() dap.step_over() end, { desc = 'Step Over' })
+vim.keymap.set("n", "<F11>", function() dap.step_into() end, { desc = 'Step Into' })
+vim.keymap.set("n", "<F12>", function() dap.step_out() end, { desc = 'Step Out' })
+vim.keymap.set("n", "<F6>", function() dap.repl.open() end, { desc = 'Replay Open' })
+
+vim.keymap.set("n", "<leader>dc", function() require 'dapui'.close() end, { desc = 'Close Debugger UI' })
 
 -- Debugger go
-vim.keymap.set("n", "<leader>dg", ":lua require('dap-go').debug_test()<CR>",
-    { desc = "[D]ebug [G]o Test" }
-)
+vim.keymap.set("n", "<leader>dg", function() require('dap-go').debug_test() end, { desc = "[D]ebug [G]o Test" })
 
 -- Debugger python
-vim.keymap.set("n", "<leader>pt", ":lua require('dap-python').test_method()<CR>",
-    { desc = "Debug [P]ythong [T]est" }
-)
-vim.keymap.set("n", "<leader>pc", ":lua require('dap-python').test_class()<CR>",
-    { desc = "Debug [P]ythong [C]lass" }
-)
-vim.keymap.set("n", "<leader>ps", ":lua require('dap-python').debug_selection()<CR>",
+local dap_python = require('dap-python')
+vim.keymap.set("n", "<leader>pt", function() dap_python.test_method() end, { desc = "Debug [P]ythong [T]est" })
+vim.keymap.set("n", "<leader>pc", function() dap_python.test_class() end, { desc = "Debug [P]ythong [C]lass" })
+vim.keymap.set("n", "<leader>ps", function() dap_python.debug_selection(opts) end,
     { desc = "Debug [P]ythong [S]election" }
 )
