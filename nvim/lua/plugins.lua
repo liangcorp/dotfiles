208b14be8ac91cbd48cbe40fd955e6425d74afd9
Chen Liang <chen.liang@ie.ibm.com>
2023-03-16 00:42:28 +0000

    Display a vscode like winbar at the top

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -72,6 +72,18 @@ require('lazy').setup({
     -- Arie (list and move between functions
     'stevearc/aerial.nvim',
 
+    {
+        "utilyre/barbecue.nvim",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons", -- optional dependency
+        },
+        opts = {
+            -- configurations go here
+        },
+    },
     -- LSP server installer and manager
     {
         'neovim/nvim-lspconfig',
