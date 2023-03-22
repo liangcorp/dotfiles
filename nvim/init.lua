c08d03f63176a6a48f4ff2eec8167b0c77e68d9c
Chen Liang <chen.liang@ie.ibm.com>
2023-03-22 13:59:05 +0000

    Set nocompatible to true

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
--- vim.o.nocompatible = true
+vim.o.nocompatible = true
 vim.o.autowrite = true
 vim.o.mouse = "a"
 vim.o.mousemodel = "popup"
