09c52705d7036440974c0fdfa1106cce3874b7dc
Chen Liang <chen.liang@ie.ibm.com>
2023-07-11 11:42:59 +0100

    Add packer bootstrap

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
@@ -227,4 +240,9 @@ return require('packer').startup(function(use)
             require("dap-python").test_runner = 'pytest'
         end
     }
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
