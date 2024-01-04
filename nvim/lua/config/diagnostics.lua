72c36e6dca61333f5bb964a67dd9a7c82ada9a6e
Chen Liang <chen.liang@ie.ibm.com>
2024-01-04 07:24:01 +0000

    Disable annoying diagnositcs message in neovim

--- nvim/lua/config/diagnostics.lua
+++ nvim/lua/config/diagnostics.lua
@@ -31,7 +31,7 @@ function PrintDiagnostics(opts, bufnr, line_nr, client_id)
     vim.api.nvim_echo({ { diagnostic_message, "Normal" } }, false, {})
 end
 
-vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
+-- vim.cmd [[ autocmd! CursorHold * lua PrintDiagnostics() ]]
 
 vim.diagnostic.config({
     virtual_text = {
