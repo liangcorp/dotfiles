137b056da315931deb5e09415563829ed7362a4a
Chen Liang <chen.liang@ie.ibm.com>
2024-01-28 22:44:51 +0000

    fix symbles

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -203,7 +203,7 @@ lspconfig.ltex.setup {
 -- this is for diagnositcs signs on the line number column
 -- use this to beautify the plain E W signs to more fun ones
 -- !important nerdfonts needs to be setup for this to work in your terminal
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+local signs = { Error = " ", Warn = " ", Hint = "󱩎 ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
