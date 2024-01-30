bf660e4ba07fbbe88c18cbad7538c60f203f814a
Chen Liang <chen.liang@ie.ibm.com>
2024-01-30 10:56:10 +0000

    enable treesitter for some files

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,16 +4,16 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    -- ft = {
-    --     "c",
-    --     "cpp",
-    --     "rust",
-    --     "go",
-    --     "lua",
-    --     "javascript",
-    --     "typescript",
-    --     "zig",
-    -- },
+    ft = {
+        "c",
+        "cpp",
+        "rust",
+        "go",
+        "lua",
+        "javascript",
+        "typescript",
+        "zig",
+    },
     config = function()
         pcall(require("nvim-treesitter.install").update({ with_sync = true }))
         require("config.treesitter")
