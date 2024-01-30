665a5350fc06e6053b50edfefa7861ca350d3bd0
Chen Liang <chen.liang@ie.ibm.com>
2024-01-30 09:28:51 +0000

    disable treesitter by default

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,19 +4,22 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
-    ft = {
-        "c",
-        "cpp",
-        "rust",
-        "go",
-        "lua",
-        "javascript",
-        "typescript",
-        "zig",
-    },
+    -- ft = {
+    --     "c",
+    --     "cpp",
+    --     "rust",
+    --     "go",
+    --     "lua",
+    --     "javascript",
+    --     "typescript",
+    --     "zig",
+    -- },
     config = function()
         pcall(require("nvim-treesitter.install").update({ with_sync = true }))
         require("config.treesitter")
     end,
     lazy = true,
+    keys = {
+        { "<leader>ts", "TSEnable highlight", { desc = '[T]ree[Sitter] Enable' } },
+    }
 }
