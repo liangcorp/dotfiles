fa1030495a756af86b9f131355d4343b411dd48c
Chen Liang <chen.liang@ie.ibm.com>
2024-02-12 10:58:53 +0000

    move go settings around

--- nvim/lua/lsp/config.lua
+++ nvim/lua/lsp/config.lua
@@ -64,21 +64,6 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
--- Go configurations
--- local go_org_import = function(wait_ms)
---     local params = vim.lsp.util.make_range_params()
---     params.context = { only = { "source.organizeImports" } }
---     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
---     for cid, res in pairs(result or {}) do
---         for _, r in pairs(res.result or {}) do
---             if r.edit then
---                 local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
---                 vim.lsp.util.apply_workspace_edit(r.edit, enc)
---             end
---         end
---     end
--- end
-
 -- rust
 lspconfig.rust_analyzer.setup {
     on_attach = on_attach,
@@ -109,6 +94,20 @@ lspconfig.rust_analyzer.setup {
 }
 
 -- golang
+-- local go_org_import = function(wait_ms)
+--     local params = vim.lsp.util.make_range_params()
+--     params.context = { only = { "source.organizeImports" } }
+--     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+--     for cid, res in pairs(result or {}) do
+--         for _, r in pairs(res.result or {}) do
+--             if r.edit then
+--                 local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+--                 vim.lsp.util.apply_workspace_edit(r.edit, enc)
+--             end
+--         end
+--     end
+-- end
+
 lspconfig.gopls.setup {
     on_attach = on_attach,
     flags = lsp_flags,
