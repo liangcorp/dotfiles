c7df2a4330bb276581309292dc908ae72af4ae6f
Chen Liang <chen.liang@ie.ibm.com>
2024-02-26 13:49:57 +0000

    remove border for completion tips

--- nvim/lua/lsp/completion.lua
+++ nvim/lua/lsp/completion.lua
@@ -18,7 +18,7 @@ cmp.setup({
         end,
     },
     window = {
-        completion = cmp.config.window.bordered(),
+        -- completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
