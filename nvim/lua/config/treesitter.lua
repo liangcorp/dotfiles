ffa0a3d71c7cd2969f8a89a9a1488afa5e6573ee
Chen Liang <chen.liang.cn@outlook.com>
2024-02-27 18:56:24 +0000

    remove neotest vim

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -15,7 +15,7 @@ require 'nvim-treesitter.configs'.setup {
     sync_install = false,
 
     -- Automatically install missing parsers when entering buffer
-    -- auto_install = true,
+    auto_install = true,
 
     -- List of parsers to ignore installing (for "all")
     -- ignore_install = { "javascript" },
