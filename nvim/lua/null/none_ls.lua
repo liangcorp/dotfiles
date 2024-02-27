0da5491383a48d1d0e1924181a913437433e6a99
Chen Liang <chen.liang@ie.ibm.com>
2024-02-27 14:47:22 +0000

    remove deprecated linters

--- nvim/lua/null/none_ls.lua
+++ nvim/lua/null/none_ls.lua
@@ -5,9 +5,7 @@ local opts = {
     sources = {
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
     },
     on_attach = function(client, bufnr)
--- nvim/lua/plugins/prettier.lua
+++ nvim/lua/plugins/prettier.lua
@@ -1,28 +1,29 @@
 return {
     "MunifTanjim/prettier.nvim",
-    ft = {
-        "markdown",
-        "javascript",
-        "typescript",
-        "yaml",
-        "yml",
-        "html",
-        "css",
-        "json"
-    },
     dependencies = {
         "neovim/nvim-lspconfig",
         "nvimtools/none-ls.nvim",
     },
-    -- keys = {
-    --     '<space>p', function()
-    --         require('prettier').format()
-    --     end, { desc = '[P]rettier' }
-    -- },
-    lazy = true,
     config = function()
         require("null.none_ls")
         require("null.prettierconfig")
         require("keymaps.prettier")
     end,
+    -- ft = {
+    --     "markdown",
+    --     "javascript",
+    --     "typescript",
+    --     "yaml",
+    --     "yml",
+    --     "html",
+    --     "css",
+    --     "json"
+    -- },
+    keys = {
+        { '<space>p', function()
+            require('prettier').format()
+        end, { desc = '[P]rettier' }
+        }
+    },
+    lazy = true,
 }
