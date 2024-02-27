be9c08e8d39c2fc1caa4cc1753f3bf4f8e631823
Chen Liang <chen.liang@ie.ibm.com>
2024-02-27 17:51:27 +0000

    remove vim test

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -2,7 +2,6 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
         "nvim-neotest/neotest-plenary",
-        "nvim-neotest/neotest-vim-test",
         "antoinemadec/FixCursorHold.nvim",
     },
     lazy = true,
@@ -12,9 +11,6 @@ return {
         require("neotest").setup({
             adapters = {
                 require("neotest-plenary"),
-                require("neotest-vim-test")({
-                    ignore_file_types = { "python", "vim", "lua" },
-                }),
                 require("neotest-go"),  -- This doesn't support lazy load
             },
         })
