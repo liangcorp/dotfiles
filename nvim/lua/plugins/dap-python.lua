3466f8d1b184de16e172a7daadf6e0205eb8b6fb
Chen Liang <chen.liang.cn@outlook.com>
2024-02-29 02:01:08 +0000

    lazy load dap-python

--- nvim/lua/plugins/dap-python.lua
+++ nvim/lua/plugins/dap-python.lua
@@ -4,7 +4,7 @@ return {
         "mfussenegger/nvim-dap",
         "rcarriga/nvim-dap-ui",
     },
-    ft = { "python" },
+    -- ft = { "python" },
     config = function()
         local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
         require("dap-python").setup(path)
