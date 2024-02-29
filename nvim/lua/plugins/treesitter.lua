b20846a31d01eeddc3b5679d98e0272a30e46c65
Chen Liang <chen.liang@ie.ibm.com>
2024-02-29 17:35:57 +0000

    lazy load treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,29 @@ return {
     dependencies = {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
+    ft = {
+        "c",
+        "cpp",
+        "go",
+        "rust",
+        "tex",
+        "markdown",
+        "yaml",
+        "json",
+        "zig",
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "java",
+        "lua",
+        "groovy",
+        "xml",
+    },
     config = function()
         pcall(require("nvim-treesitter.install").update({ with_sync = true }))
         -- require("config.autopair")
         require("config.treesitter")
     end,
-    -- lazy = true,
+    lazy = true,
 }
