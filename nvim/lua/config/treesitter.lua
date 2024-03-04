752a1cfedeac5f13c30ff71fab04a0591f19bc1e
Chen Liang <chen.liang.cn@outlook.com>
2024-03-04 15:49:42 +0000

    clean up treesitter settings

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,8 +2,9 @@ require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
     ensure_installed = {
         "c",
+        "cpp",
+        "go",
         "rust",
-        "make",
     },
 
     modules = {},
@@ -11,30 +12,26 @@ require 'nvim-treesitter.configs'.setup {
     sync_install = false,
 
     -- Automatically install missing parsers when entering buffer
-    auto_install = false,
+    auto_install = true,
 
     -- List of parsers to ignore installing (for "all")
     ignore_install = {
-        "go",
-        "python",
-        "javascript",
-        "typescript",
-        "docker",
-        "latex",
+        "groovy",
         "json",
         "yaml",
-        "groovy",
-        "java",
+        "make",
+        "bash",
+        "lua",
     },
 
     highlight = {
         -- `false` will disable the whole extension
-        enable = false,
+        enable = true,
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
         -- list of language that will be disabled
-        -- disable = { "c", "rust" },
+        disable = { "lua", "make", "yaml", "json", "groovy", "bash" },
 
         -- use_languagetree = false,
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
