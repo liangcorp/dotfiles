0778aedf39e1584f3260f588e556570ce2dfd931
Chen Liang <chen.liang.cn@outlook.com>
2024-03-05 23:45:22 +0000

    remove completion for git

--- nvim/lua/navigation/telescopeconfig.lua
+++ nvim/lua/navigation/telescopeconfig.lua
@@ -17,16 +17,12 @@ require('telescope').setup({
                 go = true,
                 rust = true,
                 c = true,
+                cpp = true,
                 json = true, -- You can set the option for specific filetypes
                 yaml = true,
             }
         },
-        fzf = {
-            fuzzy = true,             -- false will only do exact matching
-            override_generic_sorter = true, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-        },
     }
 })
+
+require('telescope').load_extension('fzf')
