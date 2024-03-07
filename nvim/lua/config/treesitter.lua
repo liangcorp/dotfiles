897598e8b46e6911929f25ea95b800c89323e39d
Chen Liang <chen.liang.cn@outlook.com>
2024-03-07 01:09:41 +0000

    enable treesitter again

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -9,7 +9,7 @@ require 'nvim-treesitter.configs'.setup {
 
     modules = {},
     -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    sync_install = true,
 
     -- Automatically install missing parsers when entering buffer
     auto_install = false,
@@ -31,7 +31,7 @@ require 'nvim-treesitter.configs'.setup {
 
     highlight = {
         -- `false` will disable the whole extension
-        enable = false,
+        enable = true,
         -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
