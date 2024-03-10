1096aa38145a051ed556af3e4046e65b71823af0
Chen Liang <chen.liang.cn@outlook.com>
2024-03-10 15:55:00 +0000

    create fedora setup script

--- fedora.sh
+++ fedora.sh
@@ -2,6 +2,19 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sudo dnf install clang go git mozilla-openh264 gnome-tweaks neovim chromium yaru-theme arc-theme wireshark gcc-g++ fd-find ripgrep reaver arpwatch hashcat netcat rkhunter gobuster hping3 ffuf whatweb nbtscan socat yersinia kismet macchanger dnsenum cryptsetup testdisk subfinder lynis foremost chntpw capstone binwalk medusa bluez bettercap yara steghide sslscan evolution-ews bzip2 texlive virt-manager emacs nmap john nikto aircrack-ng hydra parsero valgrind gdb hyperfine jetbrains-mono-fonts jetbrains-mono-nl-fonts dejavu-fonts-all python3-pip -y
+sudo dnf install clang golang mozilla-openh264 gnome-tweaks neovim chromium yaru-theme arc-theme wireshark gcc-g++ fd-find ripgrep reaver arpwatch hashcat netcat rkhunter gobuster hping3 ffuf whatweb nbtscan socat yersinia kismet macchanger dnsenum cryptsetup testdisk subfinder lynis foremost chntpw capstone binwalk medusa bluez bettercap yara steghide sslscan evolution-ews bzip2 texlive virt-manager emacs nmap john nikto aircrack-ng hydra parsero valgrind gdb hyperfine jetbrains-mono-fonts jetbrains-mono-nl-fonts dejavu-fonts-all python3-pip tcpdump -y
 
 pip install pandas wheel scikit-learn
+
+ln -s $HOME/tools/dotfiles/nvim $HOME/.config/
+ln -s $HOME/tools/dotfiles/helix $HOME/.config/
+ln -s $HOME/tools/dotfiles/alacritty $HOME/.config/
+ln -s $HOME/tools/dotfiles/.tmux.conf $HOME/
+ln -s $HOME/tools/dotfiles/bash_git.sh $HOME/.bash_git.sh
+ln -s $HOME/tools/dotfiles/vim/vimrc $HOME/.vimrc
+
+cp $HOME/.bashrc $HOME/.bashrc.bak
+echo 'source "$HOME/.bash_git.sh"' >> $HOME/.bashrc
+echo 'alias ls="ls --group-directories-first --color"' >> $HOME/.bashrc
+echo 'alias vim="nvim"' >> $HOME/.bashrc
+echo 'PATH="$HOME/.local/share/nvim/mason/bin:$HOME/.cargo/bin:$HOME/go/bin:$PATH"' >> $HOME/.bashrc
