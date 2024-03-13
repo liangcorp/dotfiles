ffb7fe62e88ed704c7489e79840bc9dd1152f750
Chen Liang <chen.liang.cn@outlook.com>
2024-03-13 23:41:34 +0000

    remove cpp from treesitter default install

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,7 +2,6 @@ require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
     ensure_installed = {
         "c",
-        "cpp",
         "python",
         "gitcommit",
     },
