6d15c2a1523bea4e5429067d3e0fa255f902ee7d
Chen Liang <chen.liang.cn@outlook.com>
2024-03-22 00:05:23 +0000

    add npm

--- fedora.sh
+++ fedora.sh
@@ -2,7 +2,7 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-sudo dnf install clang golang mozilla-openh264 gnome-tweaks neovim chromium yaru-theme arc-theme wireshark gcc-g++ fd-find ripgrep reaver arpwatch hashcat netcat rkhunter gobuster hping3 ffuf whatweb nbtscan socat yersinia kismet macchanger dnsenum cryptsetup testdisk subfinder lynis foremost chntpw capstone binwalk medusa bluez bettercap yara steghide sslscan evolution-ews bzip2 texlive virt-manager emacs nmap john nikto aircrack-ng hydra parsero valgrind gdb hyperfine jetbrains-mono-fonts jetbrains-mono-nl-fonts dejavu-fonts-all python3-pip tcpdump -y
+sudo dnf install clang golang mozilla-openh264 gnome-tweaks neovim chromium yaru-theme arc-theme wireshark gcc-g++ fd-find ripgrep reaver arpwatch hashcat netcat rkhunter gobuster hping3 ffuf whatweb nbtscan socat yersinia kismet macchanger dnsenum cryptsetup testdisk subfinder lynis foremost chntpw capstone binwalk medusa bluez bettercap yara steghide sslscan evolution-ews bzip2 texlive virt-manager emacs nmap john nikto aircrack-ng hydra parsero valgrind gdb hyperfine jetbrains-mono-fonts jetbrains-mono-nl-fonts dejavu-fonts-all python3-pip tcpdump npm -y
 
 pip install pandas wheel scikit-learn
 
