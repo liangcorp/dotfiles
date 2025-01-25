3d7321d6f86cdb06647630b6bb0284ddfbba4bf7
Chen Liang <chen.liang.cn@outlook.com>
2025-01-25 13:25:57 +0800

    update with org mode

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -17,6 +17,7 @@ require 'nvim-treesitter.configs'.setup {
 
     -- List of parsers to ignore installing (for "all")
     ignore_install = {
+        'org'
     },
 
     highlight = {
@@ -26,7 +27,7 @@ require 'nvim-treesitter.configs'.setup {
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
         -- list of language that will be disabled
-        disable = { "rust", "html", "lua", "make", "yaml", "json", "groovy", "bash", "markdown", "csv", "latex", "javascript", "typescript" },
+        -- disable = { "rust", "html", "lua", "make", "yaml", "json", "groovy", "bash", "markdown", "csv", "latex", "javascript", "typescript" },
 
         -- use_languagetree = false,
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
