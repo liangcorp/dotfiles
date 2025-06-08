4a116f905db34d06d9e34fef0250f5e84c1205d7
Chen Liang <chen.liang.cn@outlook.com>
2025-06-08 23:45:51 +0800

    add everforest to neovim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,6 +31,18 @@ require("lazy").setup({
         end,
     },
 
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+        -- Optional; default configuration will be used if setup isn't called.
+        config = function()
+            require("everforest").setup({
+                -- Your config here
+            })
+        end,
+    },
     -- {
     --     "ellisonleao/gruvbox.nvim",
     --     priority = 1000,
